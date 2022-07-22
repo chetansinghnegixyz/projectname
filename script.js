b3a94237665c150c4774cd5c4ba166b80a78ba58
chetan <chetansinghnegi512@gmail.com>
2022-07-22 20:02:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let screen=document.getElementById('screen');
+buttons=document.querySelectorAll('button');
+let screenValue=' ';
+for(list of buttons){
+    item.addEventListener('click', (e)){
+
+    }
+}
